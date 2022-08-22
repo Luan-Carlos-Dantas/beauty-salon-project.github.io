e4974273b9085dfd63d4ae4592537be24b038fc7
Luan-Carlos-Dantas <luancarlos492@gmail.com>
2022-08-21 22:15:40 -0300

    Mobile Finalizade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,66 @@
+const navMenu = document.querySelector("body");
+const toggle = document.querySelectorAll(".toggle");
+const btnBackToTop = document.querySelector("#backToTop");
+const header = document.querySelector("#header");
+const headerHeight = header.offsetHeight;
 
-const navMenu = document.querySelector('body')
-const toggle = document.querySelectorAll('.toggle')
-const btnBackToTop = document.querySelector('#backToTop')
-const header = document.querySelector('#header')
-const headerHeight = header.offsetHeight
-
-for(const element of toggle){
-  element.addEventListener('click', ()=>{
-    if(!navMenu.classList.contains('show')){
-      navMenu.classList.add('show')
-    }else{
-      navMenu.classList.remove('show')
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    if (!navMenu.classList.contains("show")) {
+      navMenu.classList.add("show");
+    } else {
+      navMenu.classList.remove("show");
     }
-  })
+  });
 }
 
-window.addEventListener('scroll', ()=>{
-  if(window.scrollY >= headerHeight){
-    header.classList.add('scroll')
-  }else{
-    header.classList.remove('scroll')
+function changeTheHeader(){
+  if (window.scrollY >= headerHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
   }
-} )
+}
 
-window.addEventListener('scroll', ()=>{
-  if(window.scrollY > 320){
-    btnBackToTop.classList.add('backToTop')
-  }else{
-    btnBackToTop.classList.remove('backToTop')
+function addBtnBackToTop(){
+  if (window.scrollY > 320) {
+    btnBackToTop.classList.add("show");
+  } else {
+    btnBackToTop.classList.remove("show");
   }
-} )
+}
 
+window.addEventListener("scroll", () => {
+  addBtnBackToTop()
+  changeTheHeader()
+});
 
 // Swipper Carrousel
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 1,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-  pagination:{
-    el: '.swiper-pagination',
+  pagination: {
+    el: ".swiper-pagination",
     dynamicBullets: true,
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
 });
 
 // Scroll Review
 
-const scrollreveal = ScrollReveal(
-  {
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset:true
-  })
+const scrollreveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
-  scrollreveal.reveal(`#home .col-a, #home .col-b, #about .col-a, #about .col-b, #services header, #services .cards .card, #testimonials header, #testimonials .testimonials, #contact header, #contact article ul li, footer .col-a, footer .col-b`)
+scrollreveal.reveal(
+  `#home .col-a, #home .col-b, #about .col-a, #about .col-b, #services header, #services .cards .card, #testimonials header, #testimonials .testimonials, #contact header, #contact article ul li, footer .col-a, footer .col-b`
+);
